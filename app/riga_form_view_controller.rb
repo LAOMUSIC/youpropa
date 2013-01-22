b19d03a3b20013994c8a5fa49e7b18c41f1624f3
paolotax <paolo.tassinari@gmail.com>
2013-01-22 14:39:07 +0100

    riga form

--- app/riga_form_view_controller.rb
+++ app/riga_form_view_controller.rb
@@ -16,7 +16,7 @@ class RigaFormViewController < UITableViewController
     super
     
     unless @riga 
-      @riga = Riga.new
+      @riga = Riga.new(quantita: 1)
       puts "new riga"
     end
      
@@ -39,8 +39,6 @@ class RigaFormViewController < UITableViewController
       case index
         when 0
           cell.textLabel.text = @libro.titolo
-        # when 1
-        #   cell.detailTextLabel.text = @riga.destinatario
       end
     end  
 
@@ -50,13 +48,47 @@ class RigaFormViewController < UITableViewController
       
       case index
         when 0
-          cell.detailTextLabel.text = @libro.prezzo_copertina
+          cell.detailTextLabel.text = @riga.quantita.to_s
         when 1
           cell.detailTextLabel.text = @libro.prezzo_consigliato
       end  
     end
   end
   
+  def prepareForEditQuantitaSegue(segue, sender:sender)
+    editController = segue.destinationViewController
+    editController.testo = @riga.quantita
+    editController.setTextChangedBlock( lambda do |text, error|
+        path = NSIndexPath.indexPathForRow(1, inSection:0)
+        cell = self.tableView.cellForRowAtIndexPath(path)
+        cell.detailTextLabel.setText(text)
+        @riga.quantita = text
+        return true
+      end
+    )
+  end
+
+  def prepareForSelectPrezzoSegue(segue, sender:sender)
+    editController = segue.destinationViewController
+    editController.appunto = @appunto
+    #editController.textView.setText(name)
+    editController.setStatoChangedBlock( lambda do |text, error|
+        path = NSIndexPath.indexPathForRow(3, inSection:0)
+        cell = self.tableView.cellForRowAtIndexPath(path)
+        cell.detailTextLabel.setText(text)
+        @appunto.status = text
+        return true
+      end
+    )
+  end
+
+  def prepareForSegue(segue, sender:sender)
+    if segue.identifier.isEqualToString("editQuantita") 
+      self.prepareForEditQuantitaSegue(segue, sender:sender)
+    elsif segue.identifier.isEqualToString("selectPrezzo") 
+      self.prepareForSelectPrezzoSegue(segue, sender:sender)
+    end
+  end
   
   def save(sender)
 
